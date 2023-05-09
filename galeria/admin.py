a1a2dd587ce9358bfce5ad8a6081a024cba9da8a
Breno Colaço <brenocolaco@gmail.com>
2023-05-09 19:08:36 -0300

    Configurando o ADMIN e add itens ao banco de dados

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
 # Register your models here.
+
+#criando uma classe de visualização
+#é necessário passar a classe para o register
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda')
+    #criar links para acessar os itens
+    list_display_links = ('id', 'nome')
+    #cria uma campo de busca
+    search_fields = ('nome',)
+
+admin.site.register(Fotografia, ListandoFotografias)
--- galeria/models.py
+++ galeria/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 
 # Create your models here.
+#Lembrar de realizar o makemigration caso faça alterações nos modelos
 class Fotografia(models.Model):
+
+    OPCOES_CATEGORIA = [
+        ('NEBULOSA', 'Nebulosa'),
+        ('ESTRELA', 'Estrela'),
+        ('GALÁXIA', 'Galáxia'),
+        ('PLANETA', 'Planeta')
+    ]
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
 
     #boa pratica
-
+ 
     def __str__(self):
         return f'Fotografia [nome={self.nome}]'
 
