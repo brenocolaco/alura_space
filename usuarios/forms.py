92aeecbad58577f845dc0f6dc6c006627369dbc7
Breno Colaço <54214580+brenocolaco@users.noreply.github.com>
2023-07-07 17:12:13 -0300

    Clean validations

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -73,4 +73,23 @@ class CadastroForms(forms.Form):
             }
         ),
     )
+    #é necessario que o nome seja desse jeito para a validação se comunicar com o django
+    def clean_nome_cadastro(self):
+        nome = self.cleaned_data.get('nome_cadastro')
 
+        if nome:
+            nome = nome.strip()
+            if ' ' in nome:
+                raise forms.ValidationError('Não é possível inserir espaços dentro do campo usuário')
+            else:
+                return nome
+
+    def clean_senha_2(self):
+        senha_1 = self.cleaned_data.get('senha_1')
+        senha_2 = self.cleaned_data.get('senha_2')
+
+        if senha_1 and senha_2:
+            if senha_1 != senha_2:
+                raise forms.ValidationError('Senhas não são iguais')
+            else:
+                return senha_2
